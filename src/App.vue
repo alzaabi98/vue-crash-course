8f732a35aae04bcc424b5b72bd7e7c860a047262
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2019-02-16 23:01:12 +0400

    vue crash course 2019

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>{{ title}}</h3>
+    <Tasks :tasks="tasks" v-on:completeTaskEvent="completeTask" v-on:deleteTaskEvent="deleteTask"></Tasks>
+    <input @keyup.enter="addNewTask" type="text" class="form-control mb-2" v-model="newTask">
+    <button v-on:click="addNewTask" class="btn btn-success btn-block">Add new Task</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Tasks from "@/components/Tasks.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Tasks
+  },
+  data() {
+    return {
+      title: " Tasks Apps",
+      newTask: null,
+      tasks: [
+        {
+          body: "GO to shop",
+          completed: false
+        },
+        {
+          body: "GO to barber",
+          completed: false
+        },
+        {
+          body: "GO to school",
+          completed: false
+        },
+        {
+          body: "GO to bed",
+          completed: false
+        }
+      ]
+    };
+  },
+
+  methods: {
+    addNewTask() {
+      this.tasks.push({
+        body: this.newTask,
+        completed: false
+      });
+      this.newTask = null;
+    },
+
+    completeTask(task) {
+      task.completed = !task.completed;
+    },
+
+    deleteTask(index) {
+      console.log("delete task " + index);
+      this.tasks.splice(index, 1);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+.card {
+  padding: 20px;
+  background: #eee;
 }
 </style>
